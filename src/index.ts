76dd887f63a1df6e49dd4ed2c93113f0d9960227
leonli <leonli@wezhuiyi.com>
2022-11-01 18:27:33 +0800

    feat: canvas写字完善

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,63 @@
 type EleType = HTMLElement;
+type LineWidthType = number;
+
+type Canvas2dType = CanvasRenderingContext2D;
+type CtxType = Canvas2dType | null;
 
 interface AttrType {
   [key: string]: string | number | boolean;
 }
 
+type LocationType = {
+  x: number;
+  y: number;
+}
+
 export interface OptionsType {
   el: EleType;
   attr: AttrType;
+  lineWidth: LineWidthType
 }
 
 class Base {
+  // 挂载元素
   protected el: EleType;
+  // 元素属性
   protected attr: AttrType;
+  // canvas 元素
+  protected canvas: HTMLCanvasElement;
+  // canvas 2d 实例
+  protected ctx: CtxType;
+  // 线条宽度
+  protected lineWidth: LineWidthType;
+  // 鼠标移动最后的位置
+  protected lastLocation: LocationType;
 
-  protected ctx: EleType;
+  // 鼠标是否被按下
+  protected isMouseDown: Boolean;
 
   constructor(options: OptionsType) {
-    console.log('Base options--->', options);
-    const { el, attr } = options;
+    const { el, attr, lineWidth } = options;
     if (!el || !(el instanceof HTMLElement)) {
       throw new Error('el 未传入”HTMLElement“类型元素');
     }
     this.el = el;
     this.attr = attr;
+    this.lineWidth = lineWidth || 10;
+    // 最后的位置
+    this.lastLocation = {
+      x: 0,
+      y: 0
+    };
+    this.isMouseDown = false;
     
     // 创建canvas元素
-    const ctx = this.createEle();
-    if (!ctx) {
+    const canvas = this.createEle();
+    if (!canvas) {
       throw new Error('创建canvas元素失败');
     }
-    this.ctx = ctx;
+    this.canvas = canvas;
+    this.ctx = canvas.getContext('2d');
     // 初始化鼠标事件
     this.initEvent();
   }
@@ -54,32 +82,85 @@ class Base {
 
   // 初始化事件监听
   initEvent() {
-    const { ctx } = this;
+    const { canvas } = this;
     const self = this;
     // 鼠标按下
-    ctx.onmousedown = function(e: MouseEvent): void{
+    canvas.onmousedown = function(e: MouseEvent): void{
       e.preventDefault();
-      console.log("鼠标按下---");
+
+      self.isMouseDown = true;
+      // 记录按下去位置，作为开始位置
+      const { x, y } = self.location(e);
+      Object.assign(self.lastLocation, { x, y });
+
       // 鼠标移动
-      ctx.onmousemove = function(e: MouseEvent): void{
+      canvas.onmousemove = function(e: MouseEvent): void{
         e.preventDefault();
-        console.log("鼠标移动---");
         self.draw(e);
       };
     };
 
     // 鼠标抬起
-    ctx.onmouseup = function(e: MouseEvent): void{
+    canvas.onmouseup = function(e: MouseEvent): void{
       e.preventDefault();
-      console.log("鼠标抬起---");
-      // 清空鼠标移动事件
-      ctx.onmousemove = null
+
+      self.isMouseDown = false;
+      // 清空鼠监移动事件
+      canvas.onmousemove = null;
     };
+
+    // 鼠标进入
+    canvas.onmouseenter = function(e: MouseEvent): void{
+      // 进入canvas元素 设置开始位置
+      const { x, y } = self.location(e);
+      Object.assign(self.lastLocation, { x, y });
+    }
+
+    // 鼠标离开
+    canvas.onmouseleave = function(e: MouseEvent): void{
+    }
+
+    // 鼠标抬起
+    document.onmouseup = function(e: MouseEvent): void {
+      // 鼠标离开canvas元素之外 鼠标抬起 清空移动事件
+      if (self.isMouseDown) {
+        self.isMouseDown = false;
+        canvas.onmousemove = null;
+      }
+    }
+
   }
 
   // 画线事件
   private draw(e: MouseEvent) {
-    console.log('draw-->', e);
+    const { ctx, lineWidth, lastLocation } = this; 
+    const { x, y } = this.location(e);
+
+    if(!ctx) return; 
+    // 设置线宽度
+    ctx.lineWidth = lineWidth;
+
+    // 
+    ctx.beginPath();
+    ctx.moveTo(lastLocation.x,lastLocation.y);
+    ctx.lineTo(x, y);
+    ctx.strokeStyle = 'red';
+    ctx.lineCap = "round"
+    ctx.lineJoin = "round"
+    ctx.stroke();
+
+    Object.assign(lastLocation, { x, y });
+
+  }
+
+  // 获取canvas元素位置
+  private location(e: MouseEvent): LocationType {
+    const { clientX, clientY } = e;
+    const { left, top } = this.canvas.getBoundingClientRect();
+    return {
+      x: clientX - left,
+      y: clientY - top
+    }
   }
 }
 
